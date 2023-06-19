23ba044e70e8d6882f8db6509f6b6e79984660e2
Brandon-Schefstad <bschefstad@gmail.com>
2023-06-19 08:52:26 -0400

    BS: Top K Frequent Elements

--- Brandon/Arrays & Hashing/Template/solution.test.ts
+++ Brandon/Arrays & Hashing/Template/solution.test.ts
@@ -2,5 +2,11 @@ import { assert, test } from 'vitest'
 import topKFrequent from './solution'
 
 test('Actual Test', () => {
-	assert.equal(topKFrequent, [0])
+	assert.deepEqual(topKFrequent([1], 1), [1])
+	assert.deepEqual(topKFrequent([1, 2, 2], 2), [1, 2])
+	assert.deepEqual(topKFrequent([1, 1, 1, 2, 2, 3], 2), [1, 2])
+	assert.deepEqual(topKFrequent([3, 0, 1, 0], 1), [0])
+	assert.deepEqual(topKFrequent([4, 1, -1, 2, -1, 2, 3], 2), [-1, 2])
+	assert.deepEqual(topKFrequent([5, 2, 5, 3, 5, 3, 1, 1, 3], 2), [5, 3])
+	assert.deepEqual(topKFrequent([5, 3, 1, 1, 1, 3, 73, 1], 2), [1, 3])
 })
--- Brandon/Arrays & Hashing/Template/solution.ts
+++ Brandon/Arrays & Hashing/Template/solution.ts
@@ -1,1 +1,33 @@
-export default function topKFrequent(nums: number[], k: number): number[] {}
+export default function topKFrequent(nums: number[], k: number): number[] {
+	// Filter for only unique values
+	const uniqueNums = nums.filter(getOnlyUniqueValues)
+	// If length === k, then those are the k-frequent values
+	if (uniqueNums.length === k) {
+		return uniqueNums
+	}
+
+	// Grab all values that appear more than once
+	const dupes = nums.filter(getOnlyDuplicateValues)
+
+	// If length > k, we keep filtering out values until we hit an array of length k.
+	/**Example -> k == 2
+   * [1,1,1,2,2,2,3,3,4] All values show up at least once
+   * [1,1,2,2,3] All values show up at least twice
+   * [1,2] All values show up at least three times
+ 
+   * return [1,2] because length === k 
+   */
+	if (dupes.length >= k) {
+		return topKFrequent(dupes, k)
+	}
+
+	return dupes
+}
+//Helper function, returns an array of unique values
+function getOnlyUniqueValues(value, index, array) {
+	return array.indexOf(value) === index
+}
+//Helper function, returns an array of duplicate values
+function getOnlyDuplicateValues(value, index, array) {
+	return array.indexOf(value) !== index
+}
