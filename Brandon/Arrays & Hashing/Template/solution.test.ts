94ed12ad8a6ec0a1fbd2741199ea38bbf40278ec
Brandon-Schefstad <bschefstad@gmail.com>
2023-06-19 07:56:10 -0400

    Adding vitest

---
+++ Brandon/Arrays & Hashing/Template/solution.test.ts
@@ -0,0 +1,6 @@
+import { assert, test } from 'vitest'
+import topKFrequent from './solution'
+
+test('Actual Test', () => {
+	assert.equal(topKFrequent, [0])
+})
---
+++ Brandon/Arrays & Hashing/Template/solution.ts
@@ -0,0 +1,1 @@
+export default function topKFrequent(nums: number[], k: number): number[] {}
